f33f0db0561c5df986fd6cfec0e2f512188a49f7
Szymonito <Szymon.Nowakowski@cyfroweszkoly.pl>
2024-11-02 20:15:29 +0100

    funkcja tworzenia obiektu

--- main.mjs
+++ main.mjs
@@ -14,4 +14,23 @@ import fs from 'fs'
         })
     }
 
-    
+    function createObject(name, age, email) {
+        const data = {
+            name, 
+            age,
+            email
+        }
+
+        let existingData = []
+
+        const filePath = './object.json'
+
+        if (fs.existsSync(filePath)) {
+            const fileData = fs.readFileSync(filepath)
+            existingData = JSON.parse(fileData)
+        }
+        existingData.push(data)
+
+        fs.writeFileSync(filepath, JSON.stringify(existingData, null, 2))
+        console.log("dodales obiekt")
+    }
